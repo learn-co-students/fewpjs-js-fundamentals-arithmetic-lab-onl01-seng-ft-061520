48604ce885fbfec1c9e8afde3e2cc74697aa25e3
fatoumatadiaby <fatimad5293@gmail.com>
2020-09-30 15:04:19 -0400

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 
-// Write your code here
+var num1 = 2
+var num2 = 31
+var multiply = num1 * num2 
+
+var random = Math.floor(Math.random()) + 1;
+
+var mod = Number.parseInt('100', 2);
+
+var max = Math.max(1,5,20)
