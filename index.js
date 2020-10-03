42038013c27eb5b39db03aecaf0aa66912be99ed
Alain <alains214@gmail.com>
2020-10-03 12:03:52 -0400

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
 // Write your code here
+let num1 = 2
+let num2 = 31
+let multiply = num1 * num2;
+
+let random =  Math.random() * 20
+
+let mod = 20%8
+
+let max = Math.max( 20, 6, 7, 5, 4)
+
+
+
+
+
