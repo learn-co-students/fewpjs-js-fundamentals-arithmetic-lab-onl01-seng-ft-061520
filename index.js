8af884ac37fca16aadab2c291a5163367a383d1d
blu3humm1ngb1rd <bethjeanlilley@gmail.com>
2020-10-02 21:21:04 -0700

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
-
 // Write your code here
+// 1 - multiply variables num1 and num2 whose results will equal 62
+let num1 = 31;
+let num2 = 2;
+const multiply = num1 * num2;
+
+//2 - generate random integer greater than 0
+let random = Math.floor(Math.random() * 1000) + 1;
+
+//3 - equaltion whose remainder = 4
+let mod = 20 % 8;
+
+//4 - will return "20" as highest number in the set
+let max = Math.max(1, 2, 3, 20);
