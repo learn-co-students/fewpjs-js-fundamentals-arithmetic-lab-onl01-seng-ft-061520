147145051987af7033837f197f27f96f873f0b2a
Amonsta <amana.ammi@gmail.com>
2020-10-05 14:16:03 -0600

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
+
+const random = Math.floor(Math.random() * 10) + 1;
+
+const mod = 19 % 5
+
+const max = Math.max(1, 2, 5, 10, 20);
+
+
