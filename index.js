c2ea2ab8132899f906ad1773774f295affdb9309
Koray Ozkal <koryozkal@gmail.com>
2020-10-17 22:49:07 +0000

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 
 // Write your code here
+
+let num1 = 1
+let num2 = 62
+let multiply = num1 * num2
+let random = Math.random() 
+let max = Math.max(20) 
+
+let num3 = 24 
+let num4 = 5
+let mod = num3 % num4
