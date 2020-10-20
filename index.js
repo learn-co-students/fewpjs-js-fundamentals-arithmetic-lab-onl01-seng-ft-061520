0347da99f74b0dbd9e500492d58309c79a327fcc
Ray O <52436369+Rockenfels@users.noreply.github.com>
2020-10-20 14:42:54 -0600

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 
 // Write your code here
+const num1 = 31;
+const num2 = 2;
+
+let multiply = num1 * num2;
+let random = Math.floor(Math.random() * 5) + 1;
+
+let mod = 28 % 8;
+let max = Math.max(1, 5, 20, 13, 9);
+ 
