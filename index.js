e01fab7d3433ca065f6839cbe3eea7dfe86cc42a
Tesfaye Atomsa <tesfayeatomsa@gmail.com>
2020-10-27 12:24:54 -0600

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
+let multiply;
+let num1 = 31
+let num2 = 2
+multiply = num1 * num2
 
-// Write your code here
+let random = Math.random() * 10
+
+let mod = 16 % 6
+
+let max = Math.max(1, 2, 3, 20)
